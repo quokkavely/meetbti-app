52f756d46d31a7139fecbe90faabf042e8baba39
Lifesheep1 <scientist5@naver.com>
2024-08-19 19:19:29 +0900

    feat ImageGameService(#47)

---
+++ server/src/main/java/com/springboot/imagegame/service/ImageGameService.java
@@ -0,0 +1,39 @@
+package com.springboot.imagegame.service;
+
+import com.springboot.imagegame.entity.ImageGame;
+import com.springboot.imagegame.repository.ImageGameRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+@Service
+public class ImageGameService {
+    private final ImageGameRepository repository;
+
+    public ImageGameService(ImageGameRepository repository) {
+        this.repository = repository;
+    }
+
+    public ImageGame createGame(ImageGame game){
+        return repository.save(game);
+    }
+    public ImageGame findGame(long gameId){
+        return findVerifiedGame(gameId);
+    }
+    public Page<ImageGame> findGames(int page, int size){
+        return repository.findAll(PageRequest.of(page, size, Sort.by("gameId").descending()));
+    }
+    public void deleteGame(long gameId){
+        ImageGame game = findVerifiedGame(gameId);
+        repository.delete(game);
+    }
+    public ImageGame findVerifiedGame(long gameId){
+        Optional<ImageGame> optionalGame = repository.findByGameId(gameId);
+        return optionalGame.orElseThrow(() -> new RuntimeException());
+    }
+}
