773e0f85503d83ca2d925de61ee7dde3912739bf
ockeee <ycgod2842@naver.com>
2024-08-19 14:55:59 +0900

    TestMainPage

---
+++ client/src/pages/TestMain.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './TestMain.css';
+import { useNavigate } from 'react-router-dom';
+
+const TestMainContent = () => {
+  const navigate = useNavigate();
+  
+  return (
+    <div className="container">
+      <div className="left-container">
+        <div className="rolling-container rolling-down">
+          <div className="image-block">
+            <img src="redgirl.jpeg" alt="Image 1" />
+          </div>
+          <div className="image-block">
+            <img src="snack-img1.jpeg" alt="Image 2" />
+          </div>
+          <div className="image-block">
+            <img src="whitegirl.jpeg" alt="Image 3" />
+          </div>
+          <div className="image-block">
+            <img src="snack-img2.jpeg" alt="Image 4" />
+          </div>
+          <div className="image-block">
+            <img src="whitegirl.jpeg" alt="Image 5" />
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+       <div className="back-button"> 
+        <img
+            src="back.png"
+            alt="back"
+            className="backButton"
+            onClick={() => window.history.back()}
+        />
+        </div>
+        <div className="rolling-container rolling-up">
+          <div className="image-block">
+            <img src="snack-img2.jpeg" alt="Image 5" />
+          </div>
+          <div className="image-block">
+            <img src="whitegirl.jpeg" alt="Image 4" />
+          </div>
+          <div className="image-block">
+            <img src="snack-img1.jpeg" alt="Image 3" />
+          </div>
+          <div className="image-block">
+            <img src="redgirl.jpeg" alt="Image 2" />
+          </div>
+          <div className="image-block">
+            <img src="whitegirl.jpeg" alt="Image 1" />
+          </div>
+        </div>
+      </div>
+      <button className="mbti-button">MBTI 검사</button>
+    </div>
+  );
+}
+
+const TestMain = () => {
+  return (
+    <div className="app">
+      <TestMainContent />
+    </div>
+  );
+};
+
+export default TestMainContent;
