759bb447382ba0e9c7b0e9095e2547f3f9f20be3
quokkavely <yellowjerri@gmail.com>
2024-08-19 16:05:07 +0900

    feat MemberDto(#70)

---
+++ server/src/main/java/com/springboot/member/mapper/MemberMapper.java
@@ -0,0 +1,19 @@
+package com.springboot.member.mapper;
+
+import com.springboot.member.dto.MemberPatchDto;
+import com.springboot.member.dto.MemberPostDto;
+import com.springboot.member.dto.MemberResponseDto;
+import com.springboot.member.entity.Member;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+
+    Member memberPostDtoToMember(MemberPostDto memberPostDto);
+    Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
+    MemberResponseDto memberToResponseDto(Member member);
+    List<MemberResponseDto> membersToResponseDto(List<Member> members);
+}
+
