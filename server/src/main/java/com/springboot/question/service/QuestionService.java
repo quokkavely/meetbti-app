4dbce2251d5829e6978b3ec4bdfcf2a8c97c1026
KoKyungBeom <kyungbum6805@gmail.com>
2024-08-19 16:42:35 +0900

    feat QuestionService(#18)

---
+++ server/src/main/java/com/springboot/question/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.springboot.question.service;
+
+import com.springboot.question.entity.Question;
+import com.springboot.question.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public List<Question> findAllQuestions () {
+        return questionRepository.findAll();
+    }
+}
