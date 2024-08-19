0f18bb4dfaf4bd672ddbbca25364393b9d4c5d2b
quokkavely <yellowjerri@gmail.com>
2024-08-19 17:53:09 +0900

    feat security authority(#92)

---
+++ server/src/main/java/com/springboot/auth/utils/ErrorResponder.java
@@ -0,0 +1,22 @@
+package com.springboot.auth.utils;
+
+import com.google.gson.Gson;
+import com.springboot.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ErrorResponder {
+    public static void sendErrorResponse(HttpServletResponse response,
+                                         HttpStatus httpStatus)
+            throws IOException {
+
+        Gson gson = new Gson();
+        ErrorResponse errorResponse = ErrorResponse.of(httpStatus);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(httpStatus.value());
+        response.getWriter().write(gson.toJson(errorResponse,ErrorResponse.class));
+    }
+}
