11c5f8148ae871a2ebe59ef63069359a4cc951da
Lifesheep1 <scientist5@naver.com>
2024-08-19 16:16:12 +0900

    LoginPage completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import TestMain from './pages/TestMain';
-import TestResult from './pages/TestResult';
-import LoginMainPage from './pages/LoginMainPage';
-import ResultWaiting from './pages/ResultWaiting';
-import MBTIBoard from './pages/MBTIBoard';
-import RegistPostPage from './pages/RegistPostPage';
+import LoginPage from './pages/loginpage/LoginPage';
+// import TestResult from './pages/TestResult';
+// import LoginMainPage from './pages/LoginMainPage';
+// import ResultWaiting from './pages/ResultWaiting';
+// import MBTIBoard from './pages/MBTIBoard';
+// import RegistPostPage from './pages/RegistPostPage';
 
 const App = () => {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/testmain" element={<TestMain />} />
-        <Route path="/resultwaiting" element={<ResultWaiting />} />
+        <Route path="/login" element={<LoginPage/>}/>
+        {/* <Route path="/resultwaiting" element={<ResultWaiting />} />
         <Route path="/testresult" element={<TestResult />} />
         <Route path="/loginmain" element={<LoginMainPage />} />
         <Route path="/mbtiboard" element={<MBTIBoard />} />
-        <Route path="/registpost" element={<RegistPostPage />} />
+        <Route path="/registpost" element={<RegistPostPage />} /> */}
       </Routes>
+    </BrowserRouter>
   );
 };
 
