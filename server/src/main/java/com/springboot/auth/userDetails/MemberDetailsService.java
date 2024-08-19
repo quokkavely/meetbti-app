8cb0ecb8fe2cdd9758c0d0238c8320c349979c33
quokkavely <yellowjerri@gmail.com>
2024-08-19 17:43:37 +0900

    feat SpringSecurityAuthentication(#91)

---
+++ server/src/main/java/com/springboot/auth/userDetails/MemberDetailsService.java
@@ -0,0 +1,78 @@
+package com.springboot.auth.userDetails;
+
+
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
+import com.springboot.member.entity.Member;
+import com.springboot.member.repository.MemberRepository;
+import com.v2.auth.utils.JwtAuthorityUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+
+@Component
+public class MemberDetailsService implements UserDetailsService {
+    public final MemberRepository memberRepository;
+    public final JwtAuthorityUtils jwtAuthorityUtils;
+
+    public MemberDetailsService(MemberRepository memberRepository, JwtAuthorityUtils jwtAuthorityUtils) {
+        this.memberRepository = memberRepository;
+        this.jwtAuthorityUtils = jwtAuthorityUtils;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember.orElseThrow(
+                ()-> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new MemberDetails(findMember);
+    }
+
+    private final class MemberDetails extends Member implements UserDetails{
+        MemberDetails(Member member){
+            setMemberId(member.getMemberId());
+            setEmail(member.getEmail());
+            setPassword(member.getPassword());
+            setRoles(member.getRoles());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return jwtAuthorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getUsername() {
+            return this.getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+
+    }
+}
