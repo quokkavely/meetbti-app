1edc3df56dcfedb91530465c171a0dc3a07342bf
Lifesheep1 <scientist5@naver.com>
2024-08-20 17:44:34 +0900

    feat MBTMIPage(#172)

---
+++ client/src/pages/mbtmi/MbtmiPage.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import './MbtmiPage.css';
+
+const MbtmiPage = () => {
+    return (
+        <div className="app">
+            <Header></Header>
+            <h3 className="mbtmi-title">INFJ의 모든 것, TMI 대방출!</h3>
+            <img src="google.png"></img>
+        </div>
+    );
+}
+export default MbtmiPage;
