e26b0ff9cc92c1024dfd0ef87298f2fb33330f8c
Lifesheep1 <scientist5@naver.com>
2024-08-21 16:35:45 +0900

    feat BalanceGameResponseDto(#189)-2트

--- server/src/main/java/com/springboot/balancegame/controller/BalanceGameController.java
+++ server/src/main/java/com/springboot/balancegame/controller/BalanceGameController.java
@@ -54,63 +54,9 @@ public class BalanceGameController {
     @GetMapping("/{game-id}")
     public ResponseEntity getGame(@PathVariable("game-id") @Positive long gameId,
                                   Authentication authentication){
-        Principal principal = (Principal) authentication.getPrincipal();
-
         BalanceGame game = balanceGameService.findGame(gameId);
 
-        // 투표 목록을 좌우로 분리
-        List<BalanceGameResult> leftResults = game.getResults().stream()
-                .filter(vote -> vote.getSelectedOption().toString().equals("L"))
-                .collect(Collectors.toList());
-        List<BalanceGameResult> rightResults = game.getResults().stream()
-                .filter(vote -> vote.getSelectedOption().toString().equals("R"))
-                .collect(Collectors.toList());
-
-        Map<String, Integer> leftCountMap = new HashMap<>();
-        Map<String, Integer> rightCountMap = new HashMap<>();
-
-        // 각 항목에서 MBTI별 득표수 구하기
-        for (BalanceGameResult vote : leftResults){
-            List<TestResult> mbtiTests = vote.getMember().getTestResults();
-            String mbti = mbtiTests.isEmpty() ? "NONE" : mbtiTests.get(mbtiTests.size() - 1).getMbti();
-
-            if(leftCountMap.containsKey(mbti)){
-                leftCountMap.put(mbti, leftCountMap.get(mbti) + 1);
-            }else{
-                leftCountMap.put(mbti, 1);
-            }
-        }
-
-        for(BalanceGameResult vote : rightResults){
-            List<TestResult> mbtiTests = vote.getMember().getTestResults();
-            String mbti = mbtiTests.isEmpty() ? "NONE" : mbtiTests.get(mbtiTests.size() - 1).getMbti();
-
-            if(rightCountMap.containsKey(mbti)){
-                rightCountMap.put(mbti, rightCountMap.get(mbti) + 1);
-            }else {
-                rightCountMap.put(mbti, 1);
-            }
-        }
-        // 각 항목을 가장 많이 고른 mbti 구하기
-        String leftMostMbti = leftCountMap.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(entry -> entry.getKey())
-                .orElse("NONE")
-                ;
-        String rightMostMbti = rightCountMap.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(entry -> entry.getKey())
-                .orElse("NONE")
-                ;
-        // 게임 참여했는지 여부
-        boolean voted = false;
-        for(BalanceGameResult result : game.getResults()){
-            if(principal.getMemberId() == result.getMember().getMemberId()){
-                voted = true;
-                break;
-            }
-        }
-        return new ResponseEntity(balanceGameMapper.gameToGameResponseDto(game, leftResults.size(), rightResults.size(),leftMostMbti, rightMostMbti, voted, balanceGameCommentMapper), HttpStatus.OK);
+        return new ResponseEntity(balanceGameMapper.gameToGameResponseDto(game, authentication, balanceGameCommentMapper), HttpStatus.OK);
     }
 
     @GetMapping
