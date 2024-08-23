55532dd0aaaa8b7b08c09c94352ceaf3f2add1c8
ockeee <ycgod2842@naver.com>
2024-08-24 02:05:08 +0900

    MainPage_MyPageModifyFix

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -35,27 +35,29 @@ const Header = (props) => {
 const MBTITest = (props) => {
   const navigate = useNavigate();
   return (
-    <div className="mbti-test-card">
-      <img src="main.png" alt="MBTI Test" className="mbti-image"/>
-      <div className="mbti-test">
-        <h2>MBTI TEST</h2>
-        <button onClick={() => {
-          navigate(props.login ? '/TestMain' : '/login');
-         console.log(props.login);
-         }}>START</button>
+    <div className="mbti-test-container">
+      <div className="mbti-test-card">
+        <img src="/public-img/main-img.png" alt="MBTI Test" className="mbti-image"/>
+        <div className="mbti-test">
+          <h2>MBTI TEST</h2>
+          <button onClick={() => {
+            navigate(props.login ? '/TestMain' : '/login');
+            console.log(props.login);
+          }}>START</button>
+        </div>
       </div>
     </div>
   )
 };
 
 // 카드 컴포넌트
-const Card = ({ image, title, description, onClick }) => {
+const Card = ({ image, title, description, onClick, className, contentClassName, titleClassName, descriptionClassName }) => {
   return (
-    <div className="card" onClick={onClick}>
+    <div className={`card ${className}`} onClick={onClick}>
       <img src={image} alt={title} className="card-image" />
-      <div className="card-content">
-        <div className="card-title">{title}</div>
-        <div className="card-description">{description}</div>
+      <div className={contentClassName ? contentClassName : "card-content"}>
+        <div className={titleClassName ? titleClassName : "card-title"}>{title}</div>
+        <div className={descriptionClassName ? descriptionClassName : "card-description"}>{description}</div>
       </div>
     </div>
   )
@@ -64,7 +66,7 @@ const Card = ({ image, title, description, onClick }) => {
 // 메인 콘텐츠 컴포넌트
 const MainContent = (props) => {
   const navigate = useNavigate();
-  const settings = {
+  const settings1 = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -72,83 +74,96 @@ const MainContent = (props) => {
     slidesToScroll: 1
   };
 
+  const settings2 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  const cards1 = [
+    { image: "/mbti-img/ENFP.png", title: "ENFP", description: "선의의 옹호자" },
+    { image: "/mbti-img/ENFJ.png", title: "ENFJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/ENTJ.png", title: "ENTJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/ENTP.png", title: "ENTP", description: "개발자의 MBTI" },
+    { image: "/mbti-img/ESFP.png", title: "ESFP", description: "선의의 옹호자" },
+    { image: "/mbti-img/ESTP.png", title: "ESTP", description: "선의의 옹호자" },
+    { image: "/mbti-img/ESFJ.png", title: "ESFJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/ESTJ.png", title: "ESTJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/INFP.png", title: "INFP", description: "선의의 옹호자" },
+    { image: "/mbti-img/INFJ.png", title: "INFJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/INTP.png", title: "INTP", description: "선의의 옹호자" },
+    { image: "/mbti-img/INTJ.png", title: "INTJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/ISFP.png", title: "ISFP", description: "선의의 옹호자" },
+    { image: "/mbti-img/ISFJ.png", title: "ISFJ", description: "선의의 옹호자" },
+    { image: "/mbti-img/ISTP.png", title: "ISTP", description: "선의의 옹호자" },
+    { image: "/mbti-img/ISTJ.png", title: "ISTJ", description: "선의의 옹호자" },
+  ];
+
+  const cards2 = [
+    { image: "/public-img/Image-img.jpeg", title: "이미지 게임", description: "가장 ~~ 할 것 같은 MBTI는?" },
+    { image: "/public-img/balance-img.jpeg", title: "밸런스 게임", description: "말해 Yes or No" },
+    { image: "/public-img/snack-img1.jpeg", title: "MBTMI", description: "MBTI의 TMI 대방출!" }
+  ];
+
+  // 배열을 랜덤으로 섞는 함수
+  const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
+
+  const shuffledCards1 = shuffleArray(cards1).slice(0, 8); // 랜덤으로 섞은 후 8개 선택
+  const shuffledCards2 = shuffleArray(cards2);
+
   return (
     <main className="main-content">
-      <MBTITest login = {props.login}/>
+      <MBTITest login={props.login} />
       <div className="other-mbti">
         <div className="other-mbti-container">
           다른 MBTI들의 생각은?!
         </div>
         <div className="go-point">
-          <img src="/public-img/to.png" alt='가기' onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}/>
+          <img src="/public-img/to.png" alt='가기' onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')} />
         </div>
       </div>
-      <Slider {...settings} className="card-list">
-        <Card 
-          image="/public-img/redgirl.jpeg" 
-          title="INFJ" 
-          description="선의의 옹호자" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
-        />
-        <Card
-          image="/public-img/whitegirl.jpeg" 
-          title="INTP" 
-          description="개발자의 MBTI" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
-        />
-        <Card 
-          image="/public-img/redgirl.jpeg" 
-          title="INFJ" 
-          description="선의의 옹호자" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')} 
-        />
-        <Card
-          image="/public-img/whitegirl.jpeg" 
-          title="INTP" 
-          description="개발자의 MBTI" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
-        />
-        <Card 
-          image="/public-img/redgirl.jpeg" 
-          title="INFJ" 
-          description="선의의 옹호자" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
-        />
-        <Card
-          image="/public-img/whitegirl.jpeg" 
-          title="INTP" 
-          description="개발자의 MBTI" 
-          onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
-        />
-      </Slider>
+      <div className="slider-container">
+        <Slider {...settings1} className="card-list">
+          {shuffledCards1.map((card, index) => (
+            <Card 
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              onClick={() => navigate(props.login ? '/MBTIBoard' : '/login')}
+            />
+          ))}
+        </Slider>
+      </div>
       <div className="snack">
         <div className="snack-title">
-        즐겨보세요, 스낵 컬처!
+          즐겨보세요, 스낵 컬처!
         </div>
         <div className="go-point">
-           <img src="/public-img/to.png" alt='가기' onClick={() => navigate(props.login ? '/snackmain' : '/login')}/>
+          <img src="/public-img/to.png" alt='가기' onClick={() => navigate(props.login ? '/snackmain' : '/login')} />
         </div>
       </div>
-      <Slider {...settings} className="card-list">
-        <Card 
-          image="/public-img/snack-img1.jpeg" 
-          title="이미지 게임" 
-          description="가장 ~~ 할 것 같은 MBTI는?"
-          onClick={() => navigate(props.login ? '/ImageGame' : '/login')}
-        />
-        <Card 
-          image="/public-img/snack-img2.jpeg" 
-          title="밸런스 게임" 
-          description="말해 Yes or No" 
-          onClick={() => navigate(props.login ? '/BalanceGame' : '/login')}
-        />
-        <Card 
-          image="/public-img/snack-img1.jpeg" 
-          title="MBTMI" 
-          description="MBTI의 TMI 대방출!" 
-          onClick={() => navigate(props.login ? '/MBTMI' : '/login')}
-        />
-      </Slider>
+      <div className="slider-container2">
+        <Slider {...settings2} className="card-list2">
+          {shuffledCards2.map((card, index) => (
+            <Card 
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              onClick={() => navigate(props.login ? '/ImageGame' : '/login')}
+              className="large-card"
+              contentClassName="card-content2"
+              titleClassName="snack-title-in"
+              descriptionClassName="snack-description-in"
+            />
+          ))}
+        </Slider>
+      </div>
     </main>
   )
 };
