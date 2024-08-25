321aea8877977d083b23836f68d41c2e81bdfd28
ockeee <ycgod2842@naver.com>
2024-08-25 18:00:37 +0900

    MBTIBoard(fix)

--- client/src/pages/mbti-board/MBTIBoard.js
+++ client/src/pages/mbti-board/MBTIBoard.js
@@ -7,27 +7,21 @@ import sendGetMyinfoRequest from '../../requests/GetMyInfo';
 
 
 // 헤더(로고, 뒤로가기) 컴포넌트
-const Header = () => {
-  const navigate = useNavigate();
-  return (
-    <header className="header">
-      <div className="logo-box">
-        <div className='logo-img' onClick={() => navigate('/')}>
-          <img src="public-img/Main-logo.png" alt='메인로고'/>
-        </div>
-        <div className="back-icon" onClick={() => navigate(-1)}>
-          <img src="public-img/back(grey).png" alt='뒤로 가기' />
-        </div>
-      </div>
-      <div className="logo-text">
-        <h1>본격 MBTI 커뮤니티!</h1>
-      </div>
-    </header>
-  );
-};
+const AppContainerComponent = () => {
+    return (
+        <AppContainer />
+    );
+  };
+  
+  const HeaderComponent = () => {
+    return (
+        <Header />
+    );
+  };
+
 
 // 필터 컴포넌트
-const Filter = () => {
+const Filter = ({ setSortOption, setMbtiType, mbtiType, sortOption }) => {
     const [isMBTIDropdownOpen, setMBTIDropdownOpen] = useState(false);
     const [isSortDropdownOpen, setSortDropdownOpen] = useState(false);
 
@@ -39,40 +33,76 @@ const Filter = () => {
         setSortDropdownOpen(!isSortDropdownOpen);
     };
 
+    const closeDropdowns = () => {
+        setMBTIDropdownOpen(false);
+        setSortDropdownOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.target.closest('.dropdown')) {
+                closeDropdowns();
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    const handleSortOptionClick = (option) => {
+        setSortOption(option);
+        closeDropdowns();
+    };
+
+    const handleMbtiTypeClick = (type) => {
+        setMbtiType(type);
+        closeDropdowns();
+    };
+
+    const sortOptionText = {
+        createdAt: '최신순',
+        popularity: '인기순',
+        views: '조회순',
+        comments: '댓글많은순'
+    };
+
     return (
         <div className="filter">
             <div className="dropdown">
-                <button className="filter-btn" onClick={toggleMBTIDropdown}>▼ INFJ</button>
+                <button className="filter-btn" onClick={toggleMBTIDropdown}>▼ {mbtiType}</button>
                 {isMBTIDropdownOpen && (
                     <div className="dropdown-menu">
-                        <button className="dropdown-item">ALL</button>
-                        <button className="dropdown-item">ENTJ</button>
-                        <button className="dropdown-item">ENTP</button>
-                        <button className="dropdown-item">ENFJ</button>
-                        <button className="dropdown-item">ENFP</button>
-                        <button className="dropdown-item">ESTJ</button>
-                        <button className="dropdown-item">ESTP</button>
-                        <button className="dropdown-item">ESFJ</button>
-                        <button className="dropdown-item">ESFP</button>
-                        <button className="dropdown-item">INTJ</button>
-                        <button className="dropdown-item">INTP</button>
-                        <button className="dropdown-item">INFJ</button>
-                        <button className="dropdown-item">INFP</button>
-                        <button className="dropdown-item">ISTJ</button>
-                        <button className="dropdown-item">ISTP</button>
-                        <button className="dropdown-item">ISFJ</button>
-                        <button className="dropdown-item">ISFP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ALL')}>ALL</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ENTJ')}>ENTJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ENTP')}>ENTP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ENFJ')}>ENFJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ENFP')}>ENFP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ESTJ')}>ESTJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ESTP')}>ESTP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ESFJ')}>ESFJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ESFP')}>ESFP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('INTJ')}>INTJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('INTP')}>INTP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('INFJ')}>INFJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('INFP')}>INFP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ISTJ')}>ISTJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ISTP')}>ISTP</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ISFJ')}>ISFJ</button>
+                        <button className="dropdown-item" onClick={() => handleMbtiTypeClick('ISFP')}>ISFP</button>
                     </div>
                 )}
             </div>
             <div className="dropdown">
-                <button className="filter-btn" onClick={toggleSortDropdown}>▼ 최신순</button>
+                <button className="filter-btn" onClick={toggleSortDropdown}>▼ {sortOptionText[sortOption]}</button>
                 {isSortDropdownOpen && (
                     <div className="dropdown-menu">
-                        <button className="dropdown-item">최신순</button>
-                        <button className="dropdown-item">인기순</button>
-                        <button className="dropdown-item">조회순</button>
-                        <button className="dropdown-item">댓글많은순</button>
+                        <button className="dropdown-item" onClick={() => handleSortOptionClick('createdAt')}>최신순</button>
+                        <button className="dropdown-item" onClick={() => handleSortOptionClick('popularity')}>인기순</button>
+                        <button className="dropdown-item" onClick={() => handleSortOptionClick('views')}>조회순</button>
+                        <button className="dropdown-item" onClick={() => handleSortOptionClick('comments')}>댓글많은순</button>
                     </div>
                 )}
             </div>
@@ -80,29 +110,38 @@ const Filter = () => {
     );
 };
 
-// 포스트 컴포넌트
-const dummyData = [
-    { title: "경범이는 치와와", views: 24200, likes: 2234, comments: 3254 },
-    { title: "원일이의 은밀한 사생활 썰 푼다.", views: 97245, likes: 9999, comments: 9999 },
-    { title: "광희랑 민준이랑 구글 갔냐?", views: 92, likes: 1, comments: 23 },
-    { title: "(실시간) 경범이 제주도 대저택 사진", views: 42875, likes: 4234, comments: 5254 },
-    { title: "럭키, 바닐라, 라떼는 사실...", views: 3855, likes: 271, comments: 2872 },
-    { title: "프로젝트 원래 이렇게 힘든거냐 ㅠㅠ", views: 74207, likes: 6754, comments: 7735 },
-];
 
 // 게시판 컴포넌트
-const Board = (props) => {
+const Board = ({ props, sortOption, mbtiType, setTotalPages }) => {
+    const { state } = useAuth();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+    const [posts, setPosts] = useState({data:[]});
+    const [myData, setMyData] = useState({data:{mbti:'ALL'}});
+
+    useEffect(() => {
+        sendGetMyinfoRequest(state, (myData) => {
+            sendGetPostsRequest(state, 1, 6, mbtiType, sortOption, setLoading, (data) => {
+                setPosts(data);
+                setTotalPages(data.totalPages); // 총 페이지 수 설정
+            });
+        });
+    }, [sortOption, mbtiType]);
+
+    if (loading) {
+        return <div> 게시판 불러오는 중...</div>;
+    }
+
     return (
         <div className="board">
             <div className="posts">
                 {!props.loading && props.posts.map((post, index) => (
                     <div key={index} className="post-item">
-                        <div className="post-title" onClick={() => navigate(`/postpage?postId=${post.postId}`)}>{post.title}</div>
+                        <div className="post-title" onClick={() => navigate(`/postpage`)}>{post.title}</div>
                         <div className="post-info">
-                            <span>조회 {post.viewCount}</span>
-                            <span>❤️ {post.heartCount}</span>
-                            <span>💬 {post.comments.length}</span>
+                            <span>조회 {post.views.toLocaleString()}</span>
+                            <span>❤️ {post.likes.toLocaleString()}</span>
+                            <span>💬 {post.comments.toLocaleString()}</span>
                         </div>
                     </div>
                 ))}
@@ -131,15 +170,17 @@ const WriteButton = (props) => {
 
 
 // 페이지 네이션 컴포넌트
-const PageNation = () => {
+const PageNation = ({ totalPages }) => {
+    const pages = Array.from({ length: Math.min(totalPages, 5) }, (_, i) => i + 1);
+
     return (
         <div className="pagination">
-                <button className="previous-page">{"<"}</button>
-                {[1, 2, 3, 4, 5].map((num) => (
-                    <button key={num} className="page-number">{num}</button>
-                ))}
-                <button className="next-page">{">"}</button>
-            </div>
+            <button className="previous-page">{"<"}</button>
+            {pages.map((num) => (
+                <button key={num} className="page-number">{num}</button>
+            ))}
+            <button className="next-page">{">"}</button>
+        </div>
     );
 };
 
@@ -154,20 +195,24 @@ const MBTIBoard = () => {
     const [posts, setPosts] = useState({data:[]});
     const location = useLocation();
     const params = new URLSearchParams(location.search);
+    const [sortOption, setSortOption] = useState('createdAt');
+    const [mbtiType, setMbtiType] = useState('ALL');
+    const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
         sendGetMyinfoRequest(state, setMyData);
-        sendGetPostsRequest(state, 1, 6, params.get('category'), 'createdAt', setLoading, setPosts);
+        sendGetPostsRequest(state, 1, 6, params.get('category'), 'createdAt', sortOption, setLoading, setPosts);
     }, []);
     
     return (
-      <div className="app">
-        <Header />
-        <Filter />
-        <Board loading = {loading} posts = {posts.data}/>
-        <WriteButton category={myData.data.mbti}/>
-        <PageNation />
-      </div>
+        <div className="app">
+          <AppContainerComponent />
+          <HeaderComponent />
+          <Filter setSortOption={setSortOption} setMbtiType={setMbtiType} mbtiType={mbtiType} sortOption={sortOption} />
+          <Board loading = {loading} posts = {posts.data} sortOption={sortOption} mbtiType={mbtiType} setTotalPages={setTotalPages} />
+          <WriteButton category={myData.data.mbti} />
+          <PageNation totalPages={totalPages} />
+        </div>
     );
   };
   
