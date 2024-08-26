2ae57a9444d068540fa6c02a5e4f09b72f65d4e2
Lifesheep1 <scientist5@naver.com>
2024-08-26 15:41:20 +0900

    feat DeletePost(#321)

---
+++ client/src/requests/DeletePostRequest.js
@@ -0,0 +1,22 @@
+const sendPostDeleteRequest = async(state, category, postId, navigate) => {
+    try{
+        const response = await fetch(`http://localhost:8080/posts/${postId}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    'content-Type': 'application/json',
+                    'Authorization': `${state.token}`
+                },
+            } 
+        );
+        if(response.ok){
+            console.log('게시글 삭제 성공');
+            navigate(`/mbtiboard?category=${category}`);
+        }else{
+            console.log('게시글 삭제 실패: ', response.status);
+        }
+    } catch (error){
+        console.error('게시글 삭제 실패', error);
+    }
+}
+export default sendPostDeleteRequest;
