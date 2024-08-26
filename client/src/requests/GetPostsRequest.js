ada06d106fc91cff1635fee0aab56ad6d50da571
Lifesheep1 <scientist5@naver.com>
2024-08-26 20:59:15 +0900

    feat MyPostList(#331)

--- client/src/requests/GetPostsRequest.js
+++ client/src/requests/GetPostsRequest.js
@@ -1,8 +1,8 @@
 const sendGetPostsRequest = async(state, page, size, category, sortBy, setLoading, setPosts) => {
     try{
-        console.log('게시글 페이지 GET요청 전송');
-        console.log(`category: ${category}, sortBy: ${sortBy}`);
-        const response = await fetch(`http://localhost:8080/posts?page=${page}&size=${size}&category=${category}&standard=${sortBy}`,
+        /* console.log(`category: ${category}, sortBy: ${sortBy}`);
+        console.log('state: ', state); */
+        const response = await fetch(`http://localhost:8080/posts?page=${page}&size=${size}&category=${category}&standard=${sortBy}&member-id=${state.memberId}`,
             {
                 method: 'GET',
                 headers: {
