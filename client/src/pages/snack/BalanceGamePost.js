65ddd9f08be821a6517b4614f043a3e950febf2a
Lifesheep1 <scientist5@naver.com>
2024-08-27 13:16:14 +0900

    feat BalancegameDetail(#354)

--- client/src/pages/snack/BalanceGamePost.js
+++ client/src/pages/snack/BalanceGamePost.js
@@ -4,6 +4,11 @@ import './BalanceGamePost.css';
 import AppContainer from '../../components/basic_css/AppContainer';
 import Header from '../../components/basic_css/Header';
 import CommentUserInfoContainer from '../../components/user_info_container/CommentUserInfoContainer';
+import { useAuth } from '../../auth/AuthContext';
+import sendGetSingleBalanceGameRequest from '../../requests/GetSingleBalancegameRequest';
+import { useLocation, useNavigate } from 'react-router-dom';
+import sendPostBalanceGameCommentRequest from '../../requests/PostBalanceGameCommentRequest';
+import sendGetMyinfoRequest from '../../requests/GetMyInfo';
 
 
 const AppContainerComponent = () => {
@@ -18,27 +23,35 @@ const HeaderComponent = () => {
     );
 };
 
-const BalancePostContainer = () => {
+const BalancePostContainer = ({ gameData, setGameData }) => {
+    const { state } = useAuth();
+    const navigate = useNavigate();
     const [inputValue, setInputValue] = useState('');
-    const [comments, setComments] = useState([
-        { text: '첫 번째 댓글입니다.', time: '2023-10-01 12:00:00' },
-        { text: '두 번째 댓글입니다.', time: '2023-10-02 13:30:00' },
-        { text: '세 번째 댓글입니다.', time: '2023-10-03 14:45:00' }
-    ]);
     const [selectedOption, setSelectedOption] = useState(null);
     const [votes, setVotes] = useState({ left: 0, right: 0 });
     const [mbtiVotes, setMbtiVotes] = useState({});
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const [isLoading, setIsLoading] = useState(true);
+    const [myData, setMyData] = useState({data:{}});
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
+    useEffect(() => {
+        sendGetMyinfoRequest(state, setMyData);
+    }, []);
+
     const handleSend = () => {
-        if (inputValue.trim()) {
-            const currentTime = new Date().toISOString().replace('T', ' ').split('.')[0];
-            setComments([...comments, { text: inputValue, time: currentTime }]);
-            setInputValue('');
+        if(myData.data.mbti === 'NONE'){
+            if(window.confirm('MBTI가 없어서 댓글을 등록할 수 없어요. 첫 테스트를 하러 가시겠어요?')){
+                navigate('/mbti-test');
+            }
         }
+        sendPostBalanceGameCommentRequest(state, params.get('gameId'), inputValue, setInputValue, 
+        () => sendGetSingleBalanceGameRequest(state, params.get('gameId'), setGameData, setIsLoading)
+    );
     };
 
     const handleVote = (option) => {
@@ -66,8 +79,8 @@ const BalancePostContainer = () => {
     return (
         <div className="balance-post-container">
             <div className="balance-post-header">
-                <div className="balance-post-title">Q. 일 할래?, 놀래?</div>
-                <div className="balance-post-writer">작성자 : 치와와</div>
+                <div className="balance-post-title">{gameData.data.title}</div>
+                <div className="balance-post-writer">{`작성자: ${gameData.nickName}`}</div>
             </div>
 
             <div className="balance-post-content">
@@ -75,7 +88,7 @@ const BalancePostContainer = () => {
                     className={`balance-post-left ${selectedOption === 'left' ? 'selected' : ''} ${selectedOption === 'left' ? 'compressed' : ''} ${selectedOption !== null ? 'compressed' : ''}`} 
                     onClick={() => handleVote('left')}
                 >
-                    월 500 받고 매일 야근하기
+                    {gameData.data.leftOption}
                     {selectedOption && (
                         <div className="vote-details">
                             <div className="vote-percentage">{leftPercentage.toFixed(2)}%</div> 
@@ -89,7 +102,7 @@ const BalancePostContainer = () => {
                     className={`balance-post-right ${selectedOption === 'right' ? 'selected' : ''} ${selectedOption === 'right' ? 'compressed' : ''} ${selectedOption !== null ? 'compressed' : ''}`} 
                     onClick={() => handleVote('right')}
                 >
-                    월 100 받고 백수생활 하기
+                    {gameData.data.rightOption}
                     {selectedOption && (
                         <div className="vote-details">
                             <div className="vote-percentage">{rightPercentage.toFixed(2)}%</div> 
@@ -104,21 +117,21 @@ const BalancePostContainer = () => {
             <div className="balance-post-count">
                 <div className="balance-post-heart-count-section">
                     <div className="balance-post-heart-img">❤️</div>
-                    <div className="balance-post-heart-count">100</div>
+                    <div className="balance-post-heart-count">{gameData.data.heartCount}</div>
                 </div>
                 <div className="balance-post-comment-section">
                     <div className="balance-post-comment-img">💬</div>
-                    <div className="balance-post-comment-count">100</div>
+                    <div className="balance-post-comment-count">{gameData.data.comments.length}</div>
                 </div>
             </div>
 
             <div className="balance-comment-container">
-                {comments.map((comment, index) => (
+                {gameData.data.comments.map((comment, index) => (
                     <div key={index} className="balance-comment-list">
                         <CommentUserInfoContainer />
                         <div className="balance-comment-section">
-                            <div className="balance-comment-text">{comment.text}</div>
-                            <div className="balance-comment-time">{comment.time}</div>
+                            <div className="balance-comment-text">{comment.content}</div>
+                            <div className="balance-comment-time">{comment.createdAt}</div>
                         </div>
                     </div>
                 ))}
@@ -131,7 +144,7 @@ const BalancePostContainer = () => {
                         className="balance-comment-input-field"
                     />
                     <div className="balance-comment-send-button" onClick={handleSend}>
-                        <img src="send-img.png" alt="댓글 보내기" />
+                        <img src="public-img/send-img.png" alt="댓글 보내기" />
                     </div>
                 </div>
             </div>
@@ -140,11 +153,20 @@ const BalancePostContainer = () => {
 };
 
 const BalanceGamePost = () => {
+    const { state } = useAuth();
+    const [gameData, setGameData] = useState({data:{comments:[]}});
+    const [isLoading, setIsLoading] = useState(true);
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+
+    useEffect(() => {
+        sendGetSingleBalanceGameRequest(state, params.get('gameId'), setGameData, setIsLoading);
+    }, []);
     return (
         <div className="app">
           <AppContainerComponent />
           <HeaderComponent />
-          <BalancePostContainer />
+          <BalancePostContainer gameData={gameData} setGameData={setGameData}/>
         </div>
       );
 };
