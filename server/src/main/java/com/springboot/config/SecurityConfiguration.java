3f653b3c46477d76a17221aeb0129cc0e3c8becc
quokkavely <yellowjerri@gmail.com>
2024-12-12 11:54:55 +0900

    cors 수정 :  https 적용

--- server/src/main/java/com/springboot/config/SecurityConfiguration.java
+++ server/src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -116,7 +116,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://dg3og7cz1f8xb.cloudfront.net","http://meetbti-client.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://meetbti.site","https://www.meetbti.site", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization","MemberId"));
